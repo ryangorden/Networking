f5dd3725231ccfae562245c60711b013b3b75636
Ryan Gorden <ryangroden@gmail.com>
2020-11-10 13:58:09 -0600

    updated unit test file

--- TDD/test_net_reability.py
+++ TDD/test_net_reability.py
@@ -7,9 +7,6 @@ def test_good_get_hosts(file='hosts.yml'):
     '''
     validate that the file is a yml file a value is  a list
     '''
-
-    if 'yml' or 'yaml'not in file:
-        sys.exit('hosts file in not yaml file or file does not exist')
     with open(file, 'r') as new_file:
         hosts= safe_load(new_file)
 
