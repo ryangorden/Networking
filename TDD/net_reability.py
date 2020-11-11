8a80d60b38ea9c5361f3ae9cebf8ad63fcfc31b9
Ryan Gorden <ryangroden@gmail.com>
2020-11-11 10:21:46 -0600

    update net reachable

--- TDD/net_reability.py
+++ TDD/net_reability.py
@@ -1,8 +1,32 @@
 import os
+import sys
 from yaml import safe_load
 
-with open('hosts.yml', 'r') as file:
-    hosts= safe_load(file)
 
-for host in hosts['Hosts']:
-    os.system('ping -c 1 ' + host)
+
+def get_host(file='reachable_hosts.yml'):
+    '''
+    function input looking yaml file with hosts.
+    will return the host in a python list.
+    '''
+
+    if 'yml' not in file:
+        sys.exit('Invalid filename: Check to see if file exist or verify it is a yaml file')
+    else:
+        with open(file, 'r') as new_file:
+            hosts= safe_load(new_file)
+    return hosts['Hosts']
+
+
+def ping_hosts(hosts_list):
+    '''
+    This function takes a list of host and  check to see
+    if they are reachable.
+    '''
+
+    for host in hosts_list:
+        os.system('ping -c 1 ' + host)
+
+if __name__ == '__main__':
+    hosts_list= get_host()
+    is_reachanble = ping_hosts(hosts_list)
--- TDD/test_net_reability.py
+++ TDD/test_net_reability.py
@@ -1,9 +1,9 @@
 import os
-import pytest
-from yaml import safe_load
 import sys
+from yaml import safe_load
 
-def test_good_get_hosts(file='hosts.yml'):
+
+def test_good_get_hosts(file='reachable_hosts.yml'):
     '''
     validate that the file is a yml file a value is  a list
     '''
@@ -11,17 +11,33 @@ def test_good_get_hosts(file='hosts.yml'):
         hosts= safe_load(new_file)
 
     assert isinstance(hosts,dict)
+    assert isinstance(hosts['Hosts'],list)
 
 
 
-def test_bad_get_hosts(file='hosts.txt'):
+def test_bad_get_hosts(file='invalid_hosts.txt'):
     '''
-    validate that the file is a yml file a value is  a list
+    validate that only yaml file is passed in.
     '''
 
-    if 'yml' or 'yaml' not in file:
-        sys.exit('Invalid file: hosts file not in yaml or does not exist')
-    with open(file, 'r') as new_file:
-        hosts= safe_load(new_file)
+    if 'yml' not in file:
+        assert 'yml' not in file
 
-    assert isinstance(hosts,dict)
+
+
+def test_good_ping_host(host_list= ['8.8.8.8','yahoo.com']):
+    '''
+    validate that a list of host was passed through
+    '''
+
+    assert isinstance(host_list,list) == True
+
+
+
+
+def test_bad_ping_host(host_list= {}):
+    '''
+    validate that a list of host was passed through
+    '''
+
+    assert isinstance(host_list,list) == False
