af312aae480501bd048770e767a7883a0713e897
Ryan Gorden <ryangroden@gmail.com>
2020-12-09 14:13:37 -0600

    list yang data-models

---
+++ netconf/ncclient/nc-capabilities.py
@@ -0,0 +1,26 @@
+from ncclient import manager
+
+# This is making the connection to the device via ssh netconf
+my_device= {"host": "10.10.20.100",
+            "port": 830,
+            "username": "developer",
+            "password": "C1sco12345",
+            "hostkey_verify": False,
+            "allow_agent": False,
+            "look_for_keys": False,
+            "device_params": {}
+            }
+
+device= manager.connect(**my_device)
+
+# device= manager.connect(host="10.10.20.100", port=830,
+#                          username="developer", password="C1sco12345",
+#                         hostkey_verify=False, device_params={},
+#                         allow_agent=False, look_for_keys=False)
+
+# we are see what method are available for the managr
+for abilities in device.server_capabilities:
+    print(abilities)
+
+# exiting from device
+device.close_session()
