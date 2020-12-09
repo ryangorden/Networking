bf223554373bd0a881de505f175311b709c68b93
Ryan Gorden <ryangroden@gmail.com>
2020-12-09 15:52:23 -0600

    get interface info

---
+++ netconf/ncclient/nc-get-interface.py
@@ -0,0 +1,37 @@
+from ncclient import manager
+import xmltodict
+
+# This is making the connection to the device via ssh netconf
+my_device= {"host": "10.10.20.100",
+            "port": 830,
+            "username": "developer",
+            "password": "C1sco12345",
+            "hostkey_verify": False,
+            "allow_agent": False,
+            "look_for_keys": False,
+            "device_params": {}
+            }
+
+interface_filter= """
+<filter>
+  <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces"  xmlns:if="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+    <interface>
+    <name>Vlan500</name>
+    </interface>
+  </interfaces>
+</filter>
+"""
+device= manager.connect(**my_device)
+
+netconf_response= device.get(interface_filter)
+
+python_response= xmltodict.parse(netconf_response.xml)["rpc-reply"]["data"]
+config_response= python_response["interfaces"]["interface"]
+
+print("all interface details")
+print(config_response)
+print(' ')
+print("Interface Name")
+print(config_response["name"]["#text"])
+# exiting from device
+device.close_session()
