27948b89bdb3df5920e9168f1f7f5de26f6b013a
Ryan Gorden <ryangroden@gmail.com>
2020-12-09 18:16:45 -0600

    netconf config script

---
+++ netconf/ncclient/IAC_Config/nc-edit-interface.py
@@ -0,0 +1,21 @@
+from router_info import my_device
+from ncclient import manager
+from jinja2 import Environment, FileSystemLoader
+from yaml import safe_load
+
+with open("filter_vars.yml", "r") as file:
+    var= safe_load(file)
+
+env= Environment(loader=FileSystemLoader("."))
+template= env.get_template("filter_template.j2")
+payload= template.render(var)
+
+print(payload)
+#exit()
+with manager.connect(**my_device) as m:
+    response= m.edit_config(payload, target= "running")
+
+print(response)
+
+
+
---
+++ netconf/ncclient/IAC_Config/router_info.py
@@ -0,0 +1,9 @@
+my_device= {"host": "10.10.20.100",
+            "port": 830,
+            "username": "developer",
+            "password": "C1sco12345",
+            "hostkey_verify": False,
+            "allow_agent": False,
+            "look_for_keys": False,
+            "device_params": {}
+            }
