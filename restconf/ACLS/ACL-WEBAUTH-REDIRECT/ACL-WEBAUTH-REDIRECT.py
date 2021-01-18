4274ee42d85779754d4b50e0092b5c4be24298a8
Ryan Gorden <ryangroden@gmail.com>
2021-01-18 12:24:59 -0600

    ISE Guest Portal redirect acl

---
+++ restconf/ACLS/ACL-WEBAUTH-REDIRECT/ACL-WEBAUTH-REDIRECT.py
@@ -0,0 +1,45 @@
+"""
+This script is used to test acl creation, verify acl and
+delete the acl. It has been tested with Cisco IOS version
+16.9.03
+"""
+
+
+
+import json
+import requests
+
+base_server= "https://ios-xe-mgmt.cisco.com"
+acls_endpoint= "/restconf/data/Cisco-IOS-XE-native:native/ip/access-list/"
+port= 9443
+
+url= f"{base_server}:{port}{acls_endpoint}"
+
+
+headers= {"Content-Type": "application/yang-data+json",
+          "Accept": "application/yang-data+json"}
+
+# username and password
+auth= ("developer","C1sco12345")
+
+# open payload file
+with open("ACL-WEBAUTH-REDIRECT.json", "r") as file:
+    payload= json.load(file)
+
+
+# send post requests to create a new acl
+resp= requests.post(url, headers=headers, auth=auth, data=json.dumps(payload), verify= False)
+print(resp.status_code)
+
+# change url endpoint to specific acl
+# send get requests to only get that acl
+# that we just created
+acl_endpoint= "/restconf/data/Cisco-IOS-XE-native:native/ip/access-list/extended=ACL-WEBAUTH-REDIRECT"
+url= f"{base_server}:{port}{acl_endpoint}"
+resp= requests.get(url, headers=headers, auth=auth, verify= False)
+print(resp.text)
+
+# Delete acl we just created
+resp= requests.delete(url, headers=headers, auth=auth, verify= False)
+print(resp.status_code)
+
