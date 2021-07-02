a232f6006c4dde0fe296628d44aec2f311e39573
Ryan Gorden <ryangroden@gmail.com>
2021-07-02 11:56:22 -0500

    add new test

--- meraki/switch/warren_easton/test_update_portAccess.py
+++ meraki/switch/warren_easton/test_update_portAccess.py
@@ -1,5 +1,6 @@
 import meraki
 import os
+from app import app
 from dotenv import load_dotenv
 from unittest import TestCase
 from pprintpp import pprint
@@ -14,5 +15,10 @@ class TestPort(TestCase):
 
         portConfig= {"vlan": 10, "portId": 11,"serial": "Q2EX-8W8H-39HZ"}
         resp = dashboard.switch.updateDeviceSwitchPort(**portConfig)
-       # pprint(resp)
+
         self.assertEqual(resp['vlan'],10)
+
+    def test_home(self):
+        with app.test_client as c:
+            resp= c.get('/')
+            self.assertEqual(resp.status_code,200)
