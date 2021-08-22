0f4260940f289f608ae89c39b483eac0734423af
Ryan Gorden <ryangroden@gmail.com>
2021-08-22 14:52:51 -0500

    all vlans

---
+++ restconf/vlans/get_vlans.py
@@ -0,0 +1,15 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+url = "https://10.10.20.100:443/restconf/data/openconfig-vlan:vlans"
+
+headers = {
+  'Content-Type': 'application/yang-data+json',
+  'Accept': 'application/yang-data+json'
+}
+
+auth= HTTPBasicAuth("developer", "C1sco12345")
+requests.packages.urllib3.disable_warnings()
+response = requests.get(url, headers=headers, auth=auth, verify=False)
+
+print(response.text)
